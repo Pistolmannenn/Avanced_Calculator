1abaec867398e55f958b92d51ab6c6aa22ccbd98
Erik <erik.gustafsson03@elev.ntig.se>
2022-09-05 15:29:49 +0200

    halft fungerbar

--- calc.js
+++ calc.js
@@ -1,9 +1,15 @@
 
-let screen = document.getElementById("screen")
-let place1 = document.getElementById("place1")
-let place2 = document.getElementById("place2")
-let way = document.getElementById("way")
-let ra = null        // skapar lite variablar
+let screen = document.getElementById("screen");
+let place = document.getElementById("place");
+const rek = [];   
+const coll = [];  
+const clos = []; 
+let anser = ""; 
+let ans1 = "";  
+let way = "";   
+let a = 0;    
+let svar = ""                           // skapar lite variablar
+
 
 
 document.getElementById("num_one").addEventListener('click', num_one);
@@ -24,65 +30,130 @@ document.getElementById("*").addEventListener('click', times);
 document.getElementById("/").addEventListener('click', devided);
 document.getElementById("ans").addEventListener('click', ans);  // lite knappar
 
-console.log(way)
+
 function num_one() {   // gör så att nummer hamnar på screenen
-    if (ra == null){
-        place1.append("1")
-        console.log("test")
-    }
-    else{
-        place2.append("1")
-        console.log("hmmmm")
-    }
-    
+    place.append("1")
+    rek.push("1")
+    console.log(rek)
 }
 function num_two() {
-    place1.append("2")
+    place.append("2")
+    rek.push("2")
+    console.log(rek)
 }
 function num_tre() {
-    place1.append("3")
+    place.append("3")
+    rek.push("3")
+    console.log(rek)
 }
 function num_fore() {
-    place1.append("4")
+    place.append("4")
+    rek.push("4")
+    console.log(rek)
 }
 function num_five() {
-    place1.append("5")
+    place.append("5")
+    rek.push("5")
+    console.log(rek)
 }
 function num_six() {
-    place1.append("6")
+    place.append("6")
+    rek.push("6")
+    console.log(rek)
 }
 function num_seven() {
-    place1.append("7")
+    place.append("7")
+    rek.push("7")
+    console.log(rek)
 }
 function num_eight() {
-    place1.append("8")
+    place.append("8")
+    rek.push("8")
+    console.log(rek)
 }
 function num_nine() {
-    place1.append("9")
+    place.append("9")
+    rek.push("9")
+    console.log(rek)
 }
 function num_zero() {
-    place1.append("0")
+    place.append("0")
+    rek.push("0")
+    console.log(rek)
 }
 function tenth() {
-    place1.append(",")
+    place.append(",")
+    rek.push(",")
+    console.log(rek)
 }
 function negativ1() {
-    place1.append("-")
+    place.append("-")
+    rek.push("-")
+    console.log(rek)
 }
 
 function negativ2() {  //gör så att det kommer +, -, * eller / på screenen
-    way.append("-")
+    place.append("-")
+    rek.push("-")
+    console.log(rek)
+    way = 2
 }
 function plus() {
-    way.append("+")
+    place.append("+")
+    rek.push("+")
+    console.log(rek)
+    way = 1
 }
 function times() {
-    way.append("*")
+    place.append("*")
+    rek.push("*")
+    console.log(rek)
+    way = 3
 }
 function devided() {
-    way.append("/")
+    place.append("/")
+    rek.push("/")
+    console.log(rek)
+    way = 4
 }
-function ans() {
-    way.append("svar")
+
+
+function ans() {  //svars funktionen
+    anser = rek.join("");
+    console.log(anser)
+    for (let i = 0; i < rek.length; i++) {
+        console.log(i)
+        if (rek[i] == "+"|| rek[i] == "-"|| rek[i] == "*"|| rek[i] == "/") {
+            ans1 = rek.slice(a,i);
+            a = i+1
+            clos.push(ans1.join(""))
+        }
+    }
+    rek.push("0")
+    ans1 = rek.slice(a,-1);
+    clos.push(ans1.join(""))
+    place.innerHTML = ""
+    if (way == 1){
+        svar = parseInt(clos[0]) + parseInt(clos[1])
+        console.log(svar)
+        place.append(svar)
+    }
+    else if (way == 2){
+        svar = parseInt(clos[0]) - parseInt(clos[1])
+        console.log(svar)
+        place.append(svar)
+    }
+    else if (way == 3){
+        svar = parseInt(clos[0]) * parseInt(clos[1])
+        console.log(svar)
+        place.append(svar)
+    }
+    else if (way == 4){
+        svar = parseInt(clos[0]) / parseInt(clos[1])
+        console.log(svar)
+        place.append(svar)
+    } 
+    console.log(svar.split(""))
 }
+
 
