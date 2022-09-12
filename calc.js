c9ee0e50e329e9460b82be7d3eca9409461f5384
Erik <erik.gustafsson03@elev.ntig.se>
2022-09-12 10:47:00 +0200

    trigenometri

--- calc.js
+++ calc.js
@@ -9,7 +9,8 @@ let ans = "";
 let slice = 0;    
 let answer = "";   
 let wayLength = 0; 
-let parCol = 0               //dena ser till att allt inom paranteserna kommer med
+let parLength = 0;
+let parCol = 0;               //dena ser till att allt inom paranteserna kommer med
 let parAnswer = "";          //dena inhäller svar ifrån paranteserna                            
 
 
@@ -23,14 +24,24 @@ document.getElementById("num_seven").addEventListener('click', function(){button
 document.getElementById("num_eight").addEventListener('click', function(){button("8")});
 document.getElementById("num_nine").addEventListener('click', function(){button("9")});
 document.getElementById("num_zero").addEventListener('click', function(){button("0")});
-document.getElementById("tenth").addEventListener('click', function(){button(",")});
+document.getElementById("tenth").addEventListener('click', function(){button(".")});
 document.getElementById("negativ").addEventListener('click', function(){button("-")});
 document.getElementById("+").addEventListener('click', function(){button("+")});
-document.getElementById("-").addEventListener('click', function(){button("-")});
+document.getElementById("-").addEventListener('click', negativ);
 document.getElementById("*").addEventListener('click', function(){button("*")});
 document.getElementById("/").addEventListener('click', function(){button("/")});
 document.getElementById("parleft").addEventListener('click', function(){button("(")}); 
-document.getElementById("parright").addEventListener('click', function(){button(")")}); // några knappar
+document.getElementById("parright").addEventListener('click', function(){button(")")});
+document.getElementById("**").addEventListener('click', function(){button("^")}); 
+document.getElementById("sqrt").addEventListener('click', function(){button("√")}); 
+document.getElementById("pi").addEventListener('click', function(){button("π")});
+document.getElementById("e").addEventListener('click', function(){button("e")});
+document.getElementById("tan").addEventListener('click', function(){button("tan")});
+document.getElementById("sin").addEventListener('click', function(){button("sin")});
+document.getElementById("cos").addEventListener('click', function(){button("cos")}); 
+document.getElementById("arktan").addEventListener('click', function(){button("arktan")});
+document.getElementById("arksin").addEventListener('click', function(){button("arksin")}); 
+document.getElementById("arkcos").addEventListener('click', function(){button("arkcos")});// några knappar
 document.getElementById("ans").addEventListener('click', anse);
 document.getElementById("clear").addEventListener('click', clear); 
 document.getElementById("back").addEventListener('click', back); 
@@ -40,6 +51,15 @@ document.getElementById("back").addEventListener('click', back);
 function button(num) {   // gör så att nummer hamnar på skärmen och i en array
     place.append(num)
     rek.push(num)
+    if (num == "√"||num == "tan"||num == "sin"||num == "cos"||num == "arktan"||num == "arksin"||num == "arkcos"){
+        place.append("(")
+        rek.push("(")
+    }
+    console.log(rek)
+}
+function negativ() {   // dena hanterar minus så det går att få tag på negativa nummer
+    place.append("-")
+    rek.push("_")
     console.log(rek)
 }
 
@@ -57,166 +77,292 @@ function back() {
 
 function anse() {  //svars funktionen
 
+
+
     for (let i = 0; i < rek.length; i++) {
-        if (rek[i] == "+"|| rek[i] == "-"|| rek[i] == "*"|| rek[i] == "/") { //fyller mina arrays way och clos med info om ekvationen
+        if (rek[i] == "+"|| rek[i] == "_"|| rek[i] == "*"|| rek[i] == "/"|| rek[i] == ")"|| rek[i] == "^") { //fyller mina arrays way och clos med info om ekvationen
             ans = rek.slice(slice,i);
             slice = i+1
             clos.push(ans.join(""))
             way.push(rek[i])
         }
-        if (rek[i] == "("|| rek[i] == ")") {
+        if (rek[i] == "("|| rek[i] == "√"|| rek[i] == "tan"|| rek[i] == "sin"|| rek[i] == "cos"|| rek[i] == "arktan"|| rek[i] == "arksin"|| rek[i] == "arkcos") {
             slice = i+1
             way.push(rek[i])
         }
+        if (rek[i] == "π") { //fyller mina arrays way och clos med info om ekvationen
+            clos.push(Math.PI)
+            slice = i+1
+        }
+        if (rek[i] == "e") { //fyller mina arrays way och clos med info om ekvationen
+            clos.push(Math.E)
+            slice = i+1
+        }
     }
     
-    rek.push("0")
-    if (rek[slice-1] == ")") {  //gör så att slutet av clos blir koräkt
-        ans = rek.slice(slice-2,-2);
-        clos.push(ans.join(""))
-    }
-    else{
-        ans = rek.slice(slice,-1);
-        clos.push(ans.join(""))
-    }
+    rek.push("")
+    ans = rek.slice(slice,-1);      //gör så att slutet av clos blir koräkt
+    clos.push(ans.join(""))
 
-    console.log("test " + clos)
-    console.log("hmmmm " + way)
+
+    console.log("test1 " + clos)
+    console.log("hmmmm1 " + way)
     for (let i = 0; i < way.length; i++) { // gör så att allt som ska hamna i paranteserna hamnar inom paranteserna
         if (way[i] == ")"){
             way[i] = "";
             parCol = 0;
             parcalc()  
-            clos[i-1] = parAnswer.toString();
+            console.log("parsvar "+ parAnswer)
+            clos[i-parLength] = parAnswer.toString();
         }
-        if (parCol == 1){
+        if (parCol == 1){                   //ser till att allt inom paranteserna kommer med
             clospar.push(clos[i]);
             waypar.push(way[i]);
             way[i] = "";
             clos[i] = "";
+            parLength ++;
         }
         if (way[i] == "("){
             clospar.push(clos[i]);
             clos[i] = "";
             way[i] = "";
             parCol = 1;
+            parLength ++;
         }  
+        if (way[i] == "√"||way[i] == "tan"||way[i] == "sin"||way[i] == "cos"||way[i] == "arktan"||way[i] == "arksin"||way[i] == "arkcos"){                 //gör så att roten ur kommer med 
+            clospar.push(clos[i]);
+            waypar.push(way[i]);
+            clos[i] = "";
+            way[i] = "";
+            clospar.unshift("");
+        }
     }
-    
+    console.log("test2 " + clos)
+    console.log("hmmmm2 " + way)
+
     wayLength = way.length
-    for (let i = 0; i < wayLength; i++){ // fixar listorna way och clos
-        if (way[i] == ""){ 
-            way.splice(i, i) 
-            i -= 1;
-        }
-        if (clos[i] == ""){
-            clos.splice(i, i) 
-            i -= 1;
-        }
-    }
-    console.log("test " + clos)
-    console.log("hmmmm " + way)
+    fixlist(way, clos, wayLength)
+    
+    console.log("test3 " + clos)
+    console.log("hmmmm3 " + way)
 
     if (way.length == 0){ // om det bara står nummer
         answer = clos[0]
     }
-    for (let i = 0; i < way.length; i++) { // räknar gånger och divition
-        if (way[i] == "*"){
-            answer = parseInt(clos[i]) * parseInt(clos[i+1]);
-            clos[i+1] = answer.toString();
-            way[i] = "";
-            clos[i] = "";       
-        }
-        if (way[i] == "/"){
-            answer = parseInt(clos[i]) / parseInt(clos[i+1]);
-            clos[i+1] = answer.toString();
-            way[i] = "";
-            clos[i] = "";
-        }
-    }
 
-    console.log("test " + clos)
-    console.log("hmmmm " + way)
     wayLength = way.length
-    for (let i = 0; i < wayLength; i++){ // fixar listorna way och clos igen
-        if (way[i] == ""){
-            clos.splice(i, i+1) 
-            way.splice(i, i+1) 
-            i -= 1;
-        }
-    }
+    answer = toThePowerOf(way, clos, wayLength, answer)             //räknar upphöjt till
     
+    console.log("test4 " + clos)
+    console.log("hmmmm4 " + way)
+    
+    wayLength = way.length
+    fixlist(way, clos, wayLength)                   //fixar listorna way och clos
 
-    console.log("test " + clos)
-    console.log("hmmmm " + way)
+    console.log("test5 " + clos)
+    console.log("hmmmm5 " + way)
 
-    for (let i = 0; i < way.length; i++) { // räknar plus och minus
-        if (way[i] == "+"){
-            answer = parseInt(clos[i]) + parseInt(clos[i+1]);
-            clos[i+1] = answer
-            way[i] = "";
-            clos[i] = "";
-            
-        }
-        if (way[i] == "-"){
-            answer = parseInt(clos[i]) - parseInt(clos[i+1]);
-            clos[i+1] = answer
-            way[i] = "";
-            clos[i] = "";
-        }
-    }
-    console.log("test " + clos)
-    console.log("hmmmm " + way)
-    place.innerHTML = ""  //skriver utt svaret och resetar 
+    wayLength = way.length
+    answer = timesAndDevided(way, clos, wayLength, answer)           //räknar diviton och gånger
+
+    console.log("test6 " + clos)
+    console.log("hmmmm6 " + way)
+
+    wayLength = way.length
+    fixlist(way, clos, wayLength)                   //fixar listorna way och clos
+
+    console.log("test7 " + clos)
+    console.log("hmmmm7 " + way)
+
+    wayLength = way.length
+    answer = plusAndMinus(way, clos, wayLength, answer)              //räknar plus och minus
+
+    console.log("test8 " + clos)
+    console.log("hmmmm8 " + way)
+
+    place.innerHTML = ""                            //skriver utt svaret och resetar 
     place.append(answer)
     way.length = 0
     rek.length = 0;
     clos.length = 0;
+    parLength = 0;
     slice = 0;
     rek.push(answer.toString())
 }
 
 function parcalc() {                            // innehåller alla räkesät för paranteser
-    for (let i = 0; i < waypar.length; i++) { // räknar gånger och divition för paranteser
-        if (waypar[i] == "*"){
-            parAnswer = parseInt(clospar[i]) * parseInt(clospar[i+1]);
-            clospar[i+1] = parAnswer.toString();
-            waypar[i] = "";
-            clospar[i] = "";
-            
-        }
-        if (waypar[i] == "/"){
-            parAnswer = parseInt(clospar[i]) / parseInt(clospar[i+1]);
-            clospar[i+1] = parAnswer.toString();
-            waypar[i] = "";
-            clospar[i] = "";
-        }
-    }
     wayLength = waypar.length
-    for (let i = 0; i < wayLength; i++){ // fixar listorna waypar och clospar
-        if (waypar[i] == ""){
-            clospar.splice(i, i) 
-            waypar.splice(i, i) 
-            i -= 1;
-        }
+    parAnswer = toThePowerOf(waypar, clospar, wayLength, parAnswer)               //räknar upphöjt till för paranteser
+    
+    wayLength = waypar.length
+    fixlist(waypar, clospar, wayLength) // fixar listorna waypar och clospar
+
+
+    if (waypar[0] == "√"||waypar[0] == "tan"||waypar[0] == "sin"||waypar[0] == "cos"||waypar[0] == "arktan"||waypar[0] == "arksin"||waypar[0] == "arkcos"){
+        clospar.unshift("");
     }
-    console.log("test co " + clospar)
-    console.log("test way " + waypar)
-    for (let i = 0; i < waypar.length; i++) { // räknar plus och minus för paranteser
-        if (waypar[i] == "+"){
-            parAnswer = parseInt(clospar[i]) + parseInt(clospar[i+1]);
-            clospar[i+1] = parAnswer
+
+    wayLength = waypar.length
+    parAnswer = timesAndDevided(waypar, clospar, wayLength, parAnswer)           //räknar diviton och gånger för parantser
+
+    wayLength = waypar.length
+    fixlist(waypar, clospar, wayLength) // fixar listorna waypar och clospar
+
+
+    if (waypar[0] == "√"||waypar[0] == "tan"||waypar[0] == "sin"||waypar[0] == "cos"||waypar[0] == "arktan"||waypar[0] == "arksin"||waypar[0] == "arkcos"){
+        clospar.unshift("");
+    }
+
+    console.log("test co1 " + clospar)
+    console.log("test way1 " + waypar)
+    wayLength = waypar.length
+    parAnswer = plusAndMinus(waypar, clospar, wayLength, parAnswer)              //räknar plus och minus för parantser
+
+
+    wayLength = waypar.length
+    fixlist(waypar, clospar, wayLength) // fixar listorna waypar och clospar
+
+
+    console.log("test co2 " + clospar)
+    console.log("test way2 " + waypar)
+
+    wayLength = waypar.length
+    for (let i = 0; i < waypar.length; i++) {                            // räknar roten ur
+        if (waypar[i] == "√"){
+            parAnswer = Math.sqrt(parseFloat(clospar[i]));
+            clospar[i+1] = parAnswer.toString();
             waypar[i] = "";
             clospar[i] = "";
             
         }
-        if (waypar[i] == "-"){
-            parAnswer = parseInt(clospar[i]) - parseInt(clospar[i+1]);
-            clospar[i+1] = parAnswer
+    }
+
+    for (let i = 0; i < waypar.length; i++) {                            // räknar tan, sin och cos
+        if (waypar[i] == "tan"){
+            parAnswer = Math.tan(parseFloat(clospar[i]));
+            clospar[i+1] = parAnswer.toString();
             waypar[i] = "";
             clospar[i] = "";
+            
+        }
+        if (waypar[i] == "sin"){
+            parAnswer = Math.sin(parseFloat(clospar[i]));
+            clospar[i+1] = parAnswer.toString();
+            waypar[i] = "";
+            clospar[i] = "";
+            
+        }
+        if (waypar[i] == "cos"){
+            parAnswer = Math.cos(parseFloat(clospar[i]));
+            clospar[i+1] = parAnswer.toString();
+            waypar[i] = "";
+            clospar[i] = "";
+            
         }
     }
+    for (let i = 0; i < waypar.length; i++) {                            // räknar arktan, arksin och arkcos
+        if (waypar[i] == "arktan"){
+            parAnswer = Math.atan(parseFloat(clospar[i]));
+            clospar[i+1] = parAnswer.toString();
+            waypar[i] = "";
+            clospar[i] = "";
+            
+        }
+        if (waypar[i] == "arksin"){
+            parAnswer = Math.asin(parseFloat(clospar[i]));
+            clospar[i+1] = parAnswer.toString();
+            waypar[i] = "";
+            clospar[i] = "";
+            
+        }
+        if (waypar[i] == "arkcos"){
+            parAnswer = Math.acos(parseFloat(clospar[i]));
+            clospar[i+1] = parAnswer.toString();
+            waypar[i] = "";
+            clospar[i] = "";
+            
+        }
+    }
+    
     waypar.length = 0
     clospar.length = 0;
 }
+
+function fixlist(waylist, closlist, length){                        // tar bort allt tommrum ifrån mina arryas
+    for (let i = 0; i < length; i++){ 
+        if (waylist[i] == ""){ 
+            if (i == 0){ 
+                waylist.shift() 
+                i -= 1;
+            }
+            else{
+                waylist.splice(i, i) 
+                i -= 1;
+            }
+        }
+        if (closlist[i] == ""){
+            if (i == 0){ 
+                closlist.shift() 
+                i -= 1;
+            }
+            else{
+                closlist.splice(i, i) 
+                i -= 1;
+            }
+        }
+    }
+}
+
+
+function timesAndDevided(waylist, closlist, length, answ){                // räknar gånger och divition
+    for (let i = 0; i < length; i++) { 
+        if (waylist[i] == "*"){
+            answ = parseFloat(closlist[i]) * parseFloat(closlist[i+1]);
+            closlist[i+1] = answ.toString();
+            waylist[i] = "";
+            closlist[i] = "";
+            
+        }
+        if (waylist[i] == "/"){
+            answ = parseFloat(closlist[i]) / parseFloat(closlist[i+1]);
+            closlist[i+1] = answ.toString();
+            waylist[i] = "";
+            closlist[i] = "";
+        }
+    }
+    return(answ)
+}
+
+
+function plusAndMinus(waylist, closlist, length, answ){                       // räknar plus och minus
+    for (let i = 0; i < length; i++) { 
+        if (waylist[i] == "+"){
+            answ = parseFloat(closlist[i]) + parseFloat(closlist[i+1]);
+            closlist[i+1] = answ
+            waylist[i] = "";
+            closlist[i] = "";
+            
+        }
+        if (waylist[i] == "_"){
+            answ = parseFloat(closlist[i]) - parseFloat(closlist[i+1]);
+            closlist[i+1] = answ
+            waylist[i] = "";
+            closlist[i] = "";
+        }
+    }
+    return(answ)
+}
+
+
+function toThePowerOf(waylist, closlist, length, answ){                        // räknar upphöjt till  
+    for (let i = 0; i < length; i++) { 
+        if (waylist[i] == "^"){
+            answ = parseFloat(closlist[i]) ** parseFloat(closlist[i+1]);
+            closlist[i+1] = answ.toString();
+            waylist[i] = "";
+            closlist[i] = "";       
+        }
+    }
+    return(answ)
+}
+
